9d8c49c95ac58484d0f97765bc6f5de98f190a59
kbvernon <kenneth.b.vernon@gmail.com>
2024-03-11 12:58:57 -0600

    rename file

--- R/summarize-terrain.R
+++ R/summarize-terrain.R
@@ -1,11 +1,9 @@
-
 #' Summarize terrain
 #' 
 #' Returns a table of terrain statistics, including average elevation and 
 #' ruggedness and the proportion of the feature with slope below 5 degrees.
 #'
-#' @param x character, the url to the remote virtual raster
-#' @param features simple features
+#' @param x simple features
 #' @param id_col character, name of column in features to use as ID in output
 #' @param processing_dir character, just where to save a temporary file
 #' 
@@ -26,15 +24,24 @@
 #'
 #' @return data.frame with four columns: id, elevation, ruggedness, and slope
 #' 
-summarize_terrain <- function(x, features, id_col, processing_dir){
+summarize_terrain <- function(x, id_col, processing_dir = tempdir()){
   
-  rr <- terra::rast(x, vsi = TRUE)
+  # location of the virtual raster pointing to the 
+  # USGS one arc second elevation data (~30 m resolution)
+  elevation_source <- file.path(
+    "https://prd-tnm.s3.amazonaws.com/StagedProducts",
+    "Elevation/1/TIFF",
+    "USGS_Seamless_DEM_1.vrt"
+  )
   
-  # convert sf to SpatVector and project to crs(x)
-  features <- terra::vect(features)
+  # connect to vrt
+  rr <- terra::rast(elevation_source, vsi = TRUE)
+  
+  # convert x to SpatVector and project to elevation crs
+  features <- terra::vect(x)
   features <- terra::project(features, terra::crs(rr))
   
-  # save to disk, so we don't have to keep it all in memory
+  # crop and save to disk, so we don't have to keep it all in memory
   rr <- terra::crop(
     rr, 
     features,
@@ -45,15 +52,16 @@ summarize_terrain <- function(x, features, id_col, processing_dir){
     gdal = c("COMPRESS=DEFLATE", "ZLEVEL=9"),
     overwrite = TRUE
   )
-
+  
   # compute terrain measures and stack
   rasters <- c(
     rr,
     terra::terrain(rr, "TRIrmsd"),
+    terra::terrain(rr, "TPI"),
     terra::terrain(rr, "slope", unit = "degrees") <= 5
   )
   
-  names(rasters) <- c("elevation", "ruggedness", "slope")
+  names(rasters) <- c("elevation", "tri_rmsd", "tpi", "p_flat")
   
   # extract values
   # uses weights to account for the approximate fraction of each cell that
