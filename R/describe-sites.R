ded7bd2849c29975ab85750dd15da14c32f5f7dd
kbvernon <kenneth.b.vernon@gmail.com>
2024-03-14 10:44:49 -0600

    change function name

--- R/describe-sites.R
+++ R/describe-sites.R
@@ -7,7 +7,7 @@ library(terra)
 library(tidyverse)
 
 # load main function for estimating terrain stats
-here("R", "describe.R") |> source()
+here("R", "summarize-terrain.R") |> source()
 
 # turn off terra's progress bar
 terraOptions(progress = 0)
@@ -34,19 +34,19 @@ sites <- here("data", "site-points.csv") |>
 
 # here we chunk the data by county and loop over the chunks
 
-site_stats <- sites |> 
+site_terrain <- sites |> 
   # using 2km buffer
   st_buffer(2000) |> 
   nest(.by = county) |> 
-  mutate(terrain = future_map(data, \(x){ describe(x, id_col = "temp_id") })) |> 
-  select(terrain) |> 
-  unnest(terrain)
+  pull(data) |> 
+  future_map(data, \(x){ summarize_terrain(x, id_col = "temp_id") }) |> 
+  bind_rows()
 
 # extract data and save ---------------------------------------------------
 
 sites |> 
   st_drop_geometry() |> 
-  left_join(site_stats, by = "temp_id") |> 
+  left_join(site_terrain, by = "temp_id") |> 
   mutate(
     level = "site",
     area_km2 = 4 * pi,
